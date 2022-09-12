ab699ff5c6eb6d08bd828d94cfb8287ddfe368c1
NightScript <maorninja@outlook.com>
2022-09-12 17:48:07 -0400

    Fix False in primeNumber

--- Python/primeNumber.py
+++ Python/primeNumber.py
@@ -1,13 +1,13 @@
 def isPrime(number):
     # 1 is neither prime nor composite number
     # 0 and below is taboo
-    if (number < 2): return false;
+    if (number < 2): return False
 
     primeNum = True;
     # looping through 2 to number-1
     for i in range(2, number//2+1):
         if number % i == 0:
-            primeNum = False;
+            primeNum = False
             break;
 
     return primeNum;
